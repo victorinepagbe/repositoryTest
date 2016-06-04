365209bd1ec593ce210695530959bc47a2143c52
Vicky <Vicky@192.168.238.1>
2016-06-04 03:38:44 +0200

    test of my first commit

---
+++ src/testRepository/BonjourClass.java
@@ -0,0 +1,10 @@
+package testRepository;
+
+public class BonjourClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("bonjour class");
+	}
+
+}
